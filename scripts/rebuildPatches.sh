64a450d0a69c6e0156f745f4ca94ab5a50bc316e
MrIvanPlays <pekov.ivan@abv.bg>
2019-09-04 11:39:55 +0300

    Apply PaperMC/Paper#2324

--- scripts/rebuildPatches.sh
+++ scripts/rebuildPatches.sh
@@ -7,24 +7,24 @@ workdir="$basedir/work"
 echo "Rebuilding patch files from current fork state..."
 git config core.safecrlf false
 
-function cleanupPatches {
-    cd "$1"
-    for patch in *.patch; do
-        echo "$patch"
-        gitver=$(tail -n 2 "$patch" | grep -ve "^$" | tail -n 1)
-        diffs=$(git diff --staged "$patch" | grep -E "^(\+|\-)" | grep -Ev "(From [a-z0-9]{32,}|\-\-\- a|\+\+\+ b|.index)")
-
-        testver=$(echo "$diffs" | tail -n 2 | grep -ve "^$" | tail -n 1 | grep "$gitver")
-        if [ "x$testver" != "x" ]; then
-            diffs=$(echo "$diffs" | sed 'N;$!P;$!D;$d')
-        fi
-
-        if [ "x$diffs" == "x" ] ; then
-            git reset HEAD "$patch" >/dev/null
-            git checkout -- "$patch" >/dev/null
-        fi
-    done
-}
+#function cleanupPatches {
+#    cd "$1"
+#    for patch in *.patch; do
+#        echo "$patch"
+#        gitver=$(tail -n 2 "$patch" | grep -ve "^$" | tail -n 1)
+#        diffs=$(git diff --staged "$patch" | grep -E "^(\+|\-)" | grep -Ev "(From [a-z0-9]{32,}|\-\-\- a|\+\+\+ b|.index)")
+#
+#        testver=$(echo "$diffs" | tail -n 2 | grep -ve "^$" | tail -n 1 | grep "$gitver")
+#        if [ "x$testver" != "x" ]; then
+#            diffs=$(echo "$diffs" | sed 'N;$!P;$!D;$d')
+#        fi
+#
+#        if [ "x$diffs" == "x" ] ; then
+#            git reset HEAD "$patch" >/dev/null
+#            git checkout -- "$patch" >/dev/null
+#        fi
+#    done
+#}
 
 function savePatches {
     what=$1
@@ -50,10 +50,11 @@ function savePatches {
 
     cd "$basedir/$target"
 
-    git format-patch --no-stat -N -o "$basedir/${what_name}-Patches/" upstream/upstream >/dev/null
+    git format-patch --zero-commit --full-index --no-signature --no-stat -N \
+        -o "$basedir/${what_name}-Patches/" upstream/upstream >/dev/null
     cd "$basedir"
     git add -A "$basedir/${what_name}-Patches"
-    cleanupPatches "$basedir/${what_name}-Patches"
+#    cleanupPatches "$basedir/${what_name}-Patches"
     echo "  Patches saved for $what to $what_name-Patches/"
 }
 
